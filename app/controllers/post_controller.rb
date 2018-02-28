59536115b433c7fe2e7c8730194beeefe80d89dd
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2018-02-27 22:17:08 -0500

    add funtion create a new post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,7 +2,23 @@ class PostController < ApplicationController
   def index
   end
 
+  def show
+  	@post = Post.find(params[:id])
+  end
+
   def new
   	@post = Post.new
   end
+
+  def create
+  	@post = Post.new(post_params)
+
+  	@post.save
+  	redirect_to @post 
+  end
+
+  private
+  	def post_params
+  		params.require(:post).permit(:title, :body)
+  	end
 end
