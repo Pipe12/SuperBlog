887aa658321e755e822370c09ee07faf33dc8e14
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2018-03-01 23:40:44 -0500

    create a funtionality delete post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -35,6 +35,13 @@ class PostController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_index_path
+  end
+
   private
   	def post_params
   		params.require(:post).permit(:title, :body)
